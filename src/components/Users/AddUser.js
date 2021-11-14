7defd5e64e1fbe2f866170dc664dcf2c164f5c85
Kunal Singhdeo <singhdeokunal11@gmail.com>
2021-11-14 23:35:50 +0530

    managing list

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -15,7 +15,7 @@ const AddUser = (props) => {
     if (+userAge < 1) {
       return;
     }
-    console.log(userName, userAge);
+    props.onAddUser(userName, userAge);
     setUserName("");
     setUserAge("");
   };
